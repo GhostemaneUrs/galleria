cafdb98e8ed208d0f244837211c0740ee48b9cde
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-12 23:41:59 -0500

    feat: update structure api galleries

--- src/pages/api/galleries.ts
+++ src/pages/api/galleries.ts
@@ -7,22 +7,16 @@ const galleriesApi = async (
   res: NextApiResponse<ResponsiveGallery>
 ): Promise<void> => {
   const { ...filters } = req.query
+
   const obj = {
     skip: Number(filters.skip ?? 1),
     limit: Number(filters.limit ?? 5),
     totalRows: galleries.length,
     totalPages: Math.ceil(galleries.length / Number(filters.limit ?? 5)),
-    galleries: galleries
-      .slice(
-        Number(filters.skip ?? 1) - 1,
-        Number(filters.skip ?? 1) + Number(filters.limit ?? 5) - 1
-      )
-      .map(item => ({
-        id: item?.id,
-        name: item?.name,
-        artist: item?.artist?.name,
-        gallery: item?.images?.gallery
-      }))
+    galleries: galleries.slice(
+      Number(filters.skip ?? 1) - 1,
+      Number(filters.skip ?? 1) + Number(filters.limit ?? 5) - 1
+    )
   }
   res.status(200).json(obj)
 }
