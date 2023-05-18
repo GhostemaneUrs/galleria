2b7bff1f46240a748cca01462c7f0b94ef3cb5ff
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-18 04:34:15 -0500

    feat: update structure index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,41 @@
-import useSWR from 'swr'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useRouter } from 'next/router'
 import { Gallery } from '@/type/galleria'
+import { useGalleries } from '@/hook/useGalleries'
 
 const Home = () => {
-  const [filters, setFilters] = useState({
-    skip: 1,
-    limit: 15
-  })
-
-  const { data } = useSWR(
-    `/api/galleries?${new URLSearchParams(
-      Object.entries(filters)
-        .map(([key, value]) => `${key}=${String(value)}`)
-        .join('&')
-    ).toString()}`,
-    async (url: string) => await fetch(url).then(async res => await res.json())
-  )
+  const router = useRouter()
+  const galleries = useGalleries()
 
   return (
-    <div className='columns-1 sm:columns-2 xl:columns-3 2xl:columns-4 sm:gap-5 mb-10'>
-      {data?.galleries.map((item: Gallery) => {
+    <div className='columns-1 sm:columns-2 lg:columns-3 xl:columns-4 2xl:columns-4 sm:gap-5 mb-10'>
+      {galleries?.data?.map((item: Gallery) => {
         return (
           <div
             key={item?.id}
-            className='w-full h-full break-inside-avoid mb-6 relative cursor-pointer hover:scale-110 transition-all duration-300'
+            className='w-full h-full break-inside-avoid mb-6 relative cursor-pointer hover:scale-105 transition-all duration-300'
+            onClick={() => {
+              router.push(`/show/${item?.id}`).catch(err => {
+                Error(err)
+              })
+            }}
           >
             <Image
               width={600}
               height={600}
               alt={item?.name}
-              src={item?.images.gallery}
+              src={item?.gallery}
               className='w-full h-full object-contain object-center'
             />
-            <div className='w-full flex flex-col justify-end items-start p-4 absolute inset-0 bg-gradient'>
-              <span className='text-white text-2xl font-bold'>
-                {item?.name}
-              </span>
-              <span className='text-white opacity-70 text-sm'>
-                {item?.artist.name}
-              </span>
+            <div className='w-full h-full p-4 absolute inset-0 bg-gradient'>
+              <div className='w-full h-full flex flex-col justify-end items-start xl:max-w-[240px]'>
+                <span className='text-white text-xl lg:text-2xl font-bold'>
+                  {item?.name}
+                </span>
+                <span className='text-white opacity-70 text-sm'>
+                  {item?.artist}
+                </span>
+              </div>
             </div>
           </div>
         )
