4d203947fa49965b3d2c1c2ad7e173d217403270
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-18 04:33:22 -0500

    feat: implement gallery provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,21 @@ import Head from 'next/head'
 import '@/style/globals.scss'
 import type { AppProps } from 'next/app'
 import { Layout } from '@/component/layout'
+import { GalleryProvider } from '@/context/galleries'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Galleria</title>
-        <link rel='icon' href='/favicon.ico' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta key='desc' name='description' content='Next.js + Tailwind CSS' />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <GalleryProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </GalleryProvider>
     </>
   )
 }
