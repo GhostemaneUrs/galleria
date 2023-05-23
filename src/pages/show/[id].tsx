8b41dcd8c8ec094abfbee8f660b3cd68ce7e1c76
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-22 19:15:30 -0500

    feat: adding types and change server side rendering test

--- src/pages/show/[id].tsx
+++ src/pages/show/[id].tsx
@@ -1,14 +1,20 @@
 import Image from 'next/image'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { IconContext } from 'react-icons'
 import { DetailGallery } from '@/type/galleria'
 import { useGalleries } from '@/hook/useGalleries'
+import { URL_GATEWAY } from '@/constant/environment'
 import { type GetServerSidePropsContext } from 'next'
 import { motion, AnimatePresence } from 'framer-motion'
-import { FiSkipBack, FiSkipForward } from 'react-icons/fi'
-import { URL_GATEWAY } from '@/constant/environment'
+import {
+  FiSkipBack,
+  FiPlayCircle,
+  FiSkipForward,
+  FiPauseCircle
+} from 'react-icons/fi'
 
-export async function getStaticProps(
+export async function getServerSideProps(
   context: GetServerSidePropsContext
 ): Promise<{ props: { gallery: DetailGallery } }> {
   const id = context.params?.id
@@ -33,34 +39,14 @@ export async function getStaticProps(
   }
 }
 
-export async function getStaticPaths() {
-  const response = await fetch(`${URL_GATEWAY}/api/galleries`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  const data = await response.json()
-  const paths = data?.data?.map((item: Record<string, string>) => {
-    return {
-      params: {
-        id: item?.id.toString()
-      }
-    }
-  })
-  return {
-    paths,
-    fallback: false
-  }
-}
-
-const Show = ({ gallery }: Record<string, any>) => {
+const Show = ({ gallery }: { gallery: DetailGallery }) => {
   const router = useRouter()
-  const { totalRows } = useGalleries()
+  const { galleries } = useGalleries()
+  const [isPlaying, setIsPlaying] = useState<boolean>(false)
 
   const handleNextPage = () => {
     const currentId = Number(router.query.id) || 1
-    if (currentId === totalRows) {
+    if (currentId === galleries?.totalRows) {
       router.push('/show/1')
     } else {
       router.push(`/show/${currentId + 1}`)
@@ -74,28 +60,29 @@ const Show = ({ gallery }: Record<string, any>) => {
 
   const calculateProgress = () => {
     const currentId = Number(router.query.id) || 1
-    return (currentId / totalRows) * 100
+    return (currentId / galleries?.totalRows) * 100
   }
 
   return (
-    <div className='w-full flex flex-col h-[calc(100vh-100px)] overflow-hidden'>
-      <div className='flex-1-auto w-full mb-14 flex justify-center items-center'>
+    <div className='w-full flex flex-col h-[calc(100vh-94px)]'>
+      <div className='flex-1-auto w-full mb-10 flex justify-center items-center h-full'>
         <AnimatePresence mode='wait'>
           <motion.div
             key={gallery?.id}
             exit={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             initial={{ opacity: 0, x: 100 }}
-            className='w-full flex justify-between h-full'
+            className='w-full flex justify-between h-full items-center'
             transition={{ duration: 0.4, ease: 'easeInOut' }}
           >
-            <div className='w-full flex relative max-w-[720px]'>
+            <div className='w-full flex relative max-w-[720px] h-full'>
               <Image
+                priority
                 width={500}
                 height={540}
                 alt={gallery?.name}
                 src={gallery?.images?.hero?.large}
-                className='w-full h-full object-cover max-w-[480px] max-h-[540px]'
+                className='w-full h-auto object-cover max-w-[480px]'
               />
               <div className='w-full max-w-[390px] bg-white absolute -right-[110px] pl-14 py-16 flex flex-col gap-3'>
                 <span className='text-5xl text-black font-bold'>
@@ -115,7 +102,7 @@ const Show = ({ gallery }: Record<string, any>) => {
                 />
               </div>
             </div>
-            <div className='w-full flex flex-col justify-center items-center relative max-w-[390px] xl:max-w-[500px] min-h-[536px]'>
+            <div className='w-full flex flex-col justify-center items-center relative max-w-[390px] xl:max-w-[500px] '>
               <div className='w-full text-start xl:text-right'>
                 <span className='text-[160px] 2xl:text-[200px] text-[#F3F3F3] leading-none'>
                   {gallery?.year}
@@ -147,6 +134,23 @@ const Show = ({ gallery }: Record<string, any>) => {
                   : 'text-[#D8D8D8]'
               } hover:text-[#D8D8D8] cursor-pointer`}
             />
+          </IconContext.Provider>
+          <IconContext.Provider value={{ size: '2rem' }}>
+            {isPlaying ? (
+              <FiPauseCircle
+                onClick={() => {
+                  setIsPlaying(!isPlaying)
+                }}
+                className='text-[#D8D8D8] cursor-pointer'
+              />
+            ) : (
+              <FiPlayCircle
+                onClick={() => {
+                  setIsPlaying(!isPlaying)
+                }}
+                className='hover:text-[#D8D8D8] cursor-pointer'
+              />
+            )}
           </IconContext.Provider>
           <IconContext.Provider value={{ size: '2rem' }}>
             <FiSkipForward
