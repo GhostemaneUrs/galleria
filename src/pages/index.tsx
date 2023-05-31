556545f29bb5428fd7441b2e4f500f5adb1df8a8
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-31 04:43:08 -0500

    feat: update structure index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,40 +8,38 @@ const Home = () => {
   const { galleries, loading } = useGalleries()
 
   return (
-    <div className='columns-1 sm:columns-2 lg:columns-3 xl:columns-4 2xl:columns-4 sm:gap-5 mb-10'>
-      {galleries?.data
-        ?.filter((gallery: Gallery) => gallery.id !== 16)
-        .map((item: Gallery) => {
-          return (
-            <div
-              key={item?.id}
-              className='w-full h-full break-inside-avoid mb-6 relative cursor-pointer hover:scale-105 transition-all duration-300'
-              onClick={() => {
-                router.push(`/show/${item?.id}`).catch(err => {
-                  Error(err)
-                })
-              }}
-            >
-              <Image
-                width={600}
-                height={600}
-                alt={item?.name}
-                src={item?.gallery}
-                className='w-full h-full object-contain object-center'
-              />
-              <div className='w-full h-full p-4 absolute inset-0 bg-gradient'>
-                <div className='w-full h-full flex flex-col justify-end items-start xl:max-w-[240px]'>
-                  <span className='text-white text-xl lg:text-2xl font-bold'>
-                    {item?.name}
-                  </span>
-                  <span className='text-white opacity-70 text-sm'>
-                    {item?.artist}
-                  </span>
-                </div>
+    <div className='columns-1 sm:columns-2 lg:columns-3 xl:columns-3 2xl:columns-3 sm:gap-5 mb-10'>
+      {galleries?.data.map((item: Gallery) => {
+        return (
+          <div
+            key={item?.id}
+            className='w-full h-full break-inside-avoid mb-6 relative cursor-pointer hover:scale-105 transition-all duration-300'
+            onClick={() => {
+              router.push(`/show/${item?.id}`).catch(err => {
+                Error(err)
+              })
+            }}
+          >
+            <Image
+              width={600}
+              height={600}
+              alt={item?.name}
+              src={item?.gallery}
+              className='w-full h-full object-contain object-center'
+            />
+            <div className='w-full h-full p-4 absolute inset-0 bg-gradient'>
+              <div className='w-full h-full flex flex-col justify-end items-start xl:max-w-[240px]'>
+                <span className='text-white text-xl lg:text-2xl font-bold'>
+                  {item?.name}
+                </span>
+                <span className='text-white opacity-70 text-sm'>
+                  {item?.artist}
+                </span>
               </div>
             </div>
-          )
-        })}
+          </div>
+        )
+      })}
     </div>
   )
 }
